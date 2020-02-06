80784dcee96f94f3986b4d6bf046ab457813accd
tove <svensson.tove@outlook.com>
2020-02-07 00:15:24 +0100

    laboratory assignment 1 complete solution

--- ass1/login_linux.c
+++ ass1/login_linux.c
@@ -12,7 +12,7 @@
 #include <sys/types.h>
 #include <crypt.h>
 /* Uncomment next line in step 2 */
-/* #include "pwent.h" */
+#include "pwent.h" 
 
 #define TRUE 1
 #define FALSE 0
@@ -22,11 +22,16 @@ void sighandler() {
 
 	/* add signalhandling routines here */
 	/* see 'man 2 signal' */
+	signal(SIGINT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+	
 }
+
 
 int main(int argc, char *argv[]) {
 
-	struct passwd *passwddata; /* this has to be redefined in step 2 */
+	mypwent *passwddata; /* this has been redefined in step 2 */
 	/* see pwent.h */
 
 	char important1[LENGTH] = "**IMPORTANT 1**";
@@ -35,7 +40,7 @@ int main(int argc, char *argv[]) {
 
 	char important2[LENGTH] = "**IMPORTANT 2**";
 
-	//char   *c_pass; //you might want to use this variable later...
+	char *c_pass; //you might want to use this variable later...
 	char prompt[] = "password: ";
 	char *user_pass;
 
@@ -52,9 +57,15 @@ int main(int argc, char *argv[]) {
 		fflush(NULL); /* Flush all  output buffers */
 		__fpurge(stdin); /* Purge any data in stdin buffer */
 
-		if (gets(user) == NULL) /* gets() is vulnerable to buffer */
+		if (fgets(user, 16, stdin) == NULL) /* gets() is vulnerable to buffer */
 			exit(0); /*  overflow attacks.  */
 
+        for (int i = LENGTH-1; i >= 0; i--){
+            if (user[i]=='\n'){
+                user[i]='\0';
+                break;
+            }
+        }
 		/* check to see if important variable is intact after input of login name - do not remove */
 		printf("Value of variable 'important 1' after input of login name: %*.*s\n",
 				LENGTH - 1, LENGTH - 1, important1);
@@ -62,22 +73,42 @@ int main(int argc, char *argv[]) {
 		 		LENGTH - 1, LENGTH - 1, important2);
 
 		user_pass = getpass(prompt);
-		passwddata = getpwnam(user);
+		passwddata = mygetpwnam(user);
 
 		if (passwddata != NULL) {
 			/* You have to encrypt user_pass for this to work */
 			/* Don't forget to include the salt */
+			c_pass = crypt(user_pass, passwddata->passwd_salt);
 
-			if (!strcmp(user_pass, passwddata->pw_passwd)) {
+			if (!strcmp(c_pass, passwddata->passwd)) {
 
 				printf(" You're in !\n");
-
+			    printf("%d failed attempts.\n", passwddata->pwfailed);
+                passwddata->pwage++;
+                passwddata->pwfailed=0;
+				mysetpwent(user, passwddata);
+                if(passwddata->pwage>10){
+                    printf("You should change your password.\n");
+                }
 				/*  check UID, see setuid(2) */
+				if(setuid(passwddata->uid)==0){
 				/*  start a shell, use execve(2) */
-
+				    execve("/bin/sh", (char*[]) {NULL}, (char*[]) {NULL});
+				} else {
+				    printf("run as root dummy\n");
+				}
+			}else {
+			    passwddata->pwfailed++;
+		        mysetpwent(user, passwddata);
+		        if(passwddata->pwfailed>4){
+		            printf("You have entered the wrong password too many times, try again in a little while. \n");
+		            sleep(20);
+		        }
 			}
 		}
 		printf("Login Incorrect \n");
+		
+
 	}
 	return 0;
 }
